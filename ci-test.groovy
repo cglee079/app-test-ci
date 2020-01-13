8219876e3d7b0866b02c40a12c659928fd9a20d3
cglee079 <cglee079@zuminternet.com>
2020-01-13 15:41:02 +0900

    ci : ci-test

--- ci-test.groovy
+++ ci-test.groovy
@@ -1,1 +1,25 @@
-change
+pipeline {
+    agent any
+
+    stages {
+
+        stage('Git Checkout') {
+            steps {
+                git(
+                        url: 'https://github.com/cglee079/ci-test-app.git',
+                        credentialsId: '578f04b4aef8b3507d02a0e4109eeb1513cc7210',
+                        branch: 'master'
+                )
+            }
+        }
+
+        stage('Build') {
+            steps {
+                println "Build Started"
+                sh "chmod +x gradlew"
+                sh "./gradlew clean build"
+                println "Build End"
+            }
+        }
+    }
+}
